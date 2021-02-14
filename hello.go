f855bf83199c77c0a6ec345e88da2ac4e1f6b101
Matty Wang <mattywang7@gmail.com>
2021-02-14 13:39:30 +0000

    add appendBytes.go

--- hello.go
+++ hello.go
@@ -1,14 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	s := []byte{'r', 'o', 'a', 'd'}
-	t := make([]byte, len(s), (cap(s) + 1) * 2)
-	for i := range s {
-		t[i] = s[i]
-	}
-	s = t
-	fmt.Println(cap(s))
-	fmt.Println(len(s))
+
 }
